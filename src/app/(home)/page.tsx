d24247c8beb744b3e9196ba0dc547f8b59f9963a
anoopraju31 <anoopraju31@gmail.com>
2023-12-01 16:52:48 +0530

    Home Page Projects Section - initial

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from './sections/Hero'
+import Projects from './sections/Projects'
 
 export default function Home() {
 	return (
 		<main className='h-[200vh] bg-dark-blue text-white'>
 			<Hero />
+			<Projects />
 		</main>
 	)
 }
