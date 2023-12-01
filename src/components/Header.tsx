730185c3b370ea395e50adb337378dbe4aec7eea
anoopraju31 <anoopraju31@gmail.com>
2023-12-01 10:41:55 +0530

    Header Component - styled refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,14 +17,14 @@ const Header = () => {
 			<header className='fixed top-0 left-0 right-0 z-50 w-full max-w-[1400px] mx-auto mt-5 px-[10px] md:px-[25px] flex justify-between items-center'>
 				<Link
 					href='/'
-					className={`uppercase text-white text-2xl ${gantari.className}`}>
+					className={`uppercase text-white  hover:text-light-green transition-colors duration-300 text-2xl ${gantari.className}`}>
 					Anoopfolio
 				</Link>
 
 				<button
 					type='button'
 					onClick={toggleMenu}
-					className='rounded-3xl text-2xl p-1.5 flex justify-center items-center bg-white'>
+					className='rounded-3xl text-2xl p-1.5 flex justify-center items-center hover:text-white bg-white hover:bg-light-green transition-colors duration-300'>
 					<span className='sr-only'> Menu </span>
 					{isMenuOpen ? <CgClose /> : <CgMenu />}
 				</button>
