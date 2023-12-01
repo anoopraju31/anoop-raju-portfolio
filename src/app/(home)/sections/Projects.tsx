1be45be9717667524b0d009227f4b53e7995e2ef
anoopraju31 <anoopraju31@gmail.com>
2023-12-01 19:43:31 +0530

    Project Card Component - image added

--- src/app/(home)/sections/Projects.tsx
+++ src/app/(home)/sections/Projects.tsx
@@ -1,15 +1,25 @@
 import Link from 'next/link'
-import React from 'react'
+import ProjectCard from '@/components/ProjectCard'
 
 const Projects = () => {
 	return (
 		<section className='pb-10 z-50'>
 			<div className='py-5 px-[10px] flex flex-col lg:flex-row gap-4'>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 bg-red-500 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'></div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 bg-red-500 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'></div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 bg-red-500 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'></div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 bg-red-500 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'></div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 bg-red-500 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'></div>
+				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+					<ProjectCard img='/dropbox.png' />
+				</div>
+				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+					<ProjectCard img='/eat-curious.png' />
+				</div>
+				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+					<ProjectCard img='/netflix-gpt.png' />
+				</div>
+				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+					<ProjectCard img='/nike.png' />
+				</div>
+				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+					<ProjectCard img='/summerize.png' />
+				</div>
 			</div>
 
 			<div className='py-5 lg:py-10  flex justify-center items-center'>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Image from 'next/image'
 
-const ProjectCard = () => {
-	return <div>ProjectCard</div>
+const ProjectCard = ({ img }: { img: string }) => {
+	return (
+		<div className='w-full h-full'>
+			<Image
+				className='w-full h-full object-cover rounded-xl'
+				src={img}
+				alt='eat curious'
+				width={1000}
+				height={1000}
+			/>
+		</div>
+	)
 }
 
 export default ProjectCard
