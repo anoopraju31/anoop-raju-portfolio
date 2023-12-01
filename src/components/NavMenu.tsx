98b1a4ee679410d09c0068a65a7190a3bb2c3de1
anoopraju31 <anoopraju31@gmail.com>
2023-12-01 10:36:55 +0530

    Nav Menu Component - styled

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
 
 const NavMenu = () => {
-	return <div>NavMenu</div>
+	return (
+		<div className='h-screen bg-dark-blue flex justify-center items-center'>
+			<nav className='flex flex-col justify-center items-center text-white'>
+				<Link
+					href='/'
+					className='text-[8vh] lg:text-[15vh] font-bold leading-none uppercase hover:scale-75 hover:text-light-green transition-all duration-300'>
+					Home
+				</Link>
+				<Link
+					href='/projects'
+					className='text-[8vh] lg:text-[15vh] font-bold leading-none uppercase hover:scale-75 hover:text-light-green transition-all duration-300'>
+					Projects
+				</Link>
+				<Link
+					href='/contact'
+					className='text-[8vh] lg:text-[15vh] font-bold leading-none uppercase hover:scale-75 hover:text-light-green transition-all duration-300'>
+					Contact
+				</Link>
+			</nav>
+		</div>
+	)
 }
 
 export default NavMenu
