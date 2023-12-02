bb96efb9eaeb77c0c03a007bef0265d9bb1a5421
anoopraju31 <anoopraju31@gmail.com>
2023-12-02 06:07:04 +0530

    About Component - image added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import About from './sections/About'
 import Hero from './sections/Hero'
 import Projects from './sections/Projects'
 
@@ -6,6 +7,7 @@ export default function Home() {
 		<main className='bg-black text-white'>
 			<Hero />
 			<Projects />
+			<About />
 		</main>
 	)
 }
--- src/app/(home)/sections/About.tsx
+++ src/app/(home)/sections/About.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
-	return <div>About</div>
+	return (
+		<section className='py-10 md:py-[60px] lg:py-[100px] lg:px-[10px] whitespace-normal break-normal'>
+			<div className='max-w-[1400px] mx-auto py-5 px-[30px] grid grid-cols-1 lg:grid-cols-2 gap-5 justify-center'>
+				<div className='flex flex-col justify-start items-start'>
+					<div className='relative h-[400px] md:h-[500px] lg:h-full overflow-hidden'>
+						<Image
+							src='/anoop-raju.jpg'
+							className='object-cover'
+							alt=''
+							width={600}
+							height={500}
+						/>
+						<div className='w-[150px] h-[150px] bg-black absolute top-auto bottom-0 right-0 left-auto' />
+						<div className='w-[150px] h-[150px] bg-black absolute top-0 bottom-auto right-auto left-0' />
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default About
