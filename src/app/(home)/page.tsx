edf0c2eb9b2999224107e007ef00e88bf6db71dd
anoopraju31 <anoopraju31@gmail.com>
2023-12-03 13:24:38 +0530

    Hero Section moved to hero folder

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import About from './sections/About'
-import Hero from './sections/Hero'
+import Hero from './sections/hero/Hero'
 import Projects from './sections/Projects'
 
 export default function Home() {
