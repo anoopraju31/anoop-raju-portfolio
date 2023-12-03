a8c62d44569980e7db618883b0c18b3651fbe1fd
anoopraju31 <anoopraju31@gmail.com>
2023-12-03 13:31:12 +0530

    Hero Body Component styled and added to Hero section

--- src/app/(home)/sections/hero/Hero.tsx
+++ src/app/(home)/sections/hero/Hero.tsx
@@ -1,35 +1,10 @@
-import Image from 'next/image'
-import React from 'react'
+import HeroBody from './HeroBody'
+import styles from './hero.module.css'
 
 const Hero = () => {
 	return (
-		<section className='h-[65vh] md:h-[80vh] lg:h-[90vh] md:py-[50px] lg:py-[100px] px-[2%] relative overflow-hidden flex justify-center items-center'>
-			<div className='p-5 grid grid-cols-1 gap-5'>
-				<div className='flex justify-center items-center gap-[10px]'>
-					<h1 className='uppercase font-bold leading-none text-hero-text xl:text-[17em]'>
-						Anoop
-					</h1>
-					<Image
-						src='/anoop-raju.jpg'
-						className='flex-shrink-0 rounded-full overflow-hidden w-[var(--hero-image-width)] xl:w-[240px] h-[var(--hero-image-width)] xl:h-[240px] object-cover'
-						alt=''
-						width={240}
-						height={240}
-					/>
-					<h1 className='uppercase font-bold leading-none text-hero-text xl:text-[17em]'>
-						Raju
-					</h1>
-				</div>
-
-				<div className='flex justify-between items-center gap-5 uppercase'>
-					<h2 className='w-1/2 flex-shrink-0 text-center text-base sm:text-2xl lg:text-[2rem] leading-none'>
-						Web Developer
-					</h2>
-					<h2 className='w-1/2 flex-shrink-0 text-center text-base sm:text-2xl lg:text-[2rem] leading-none'>
-						Living in Kerala
-					</h2>
-				</div>
-			</div>
+		<section className={styles.main}>
+			<HeroBody />
 		</section>
 	)
 }
--- src/app/(home)/sections/hero/HeroBody.tsx
+++ src/app/(home)/sections/hero/HeroBody.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './hero.module.css'
 
 const HeroBody = () => {
-	return <div>HeroBody</div>
+	return (
+		<div className={styles.body}>
+			<div className={styles['hero-container']}>
+				<div className={styles['hero-heading-container']}>
+					<h1 className={styles['hero-heading']}>Anoop</h1>
+					<Image
+						src='/anoop-raju.jpg'
+						className='flex-shrink-0 rounded-full overflow-hidden w-[var(--hero-image-width)] xl:w-[240px] h-[var(--hero-image-width)] xl:h-[240px] object-cover'
+						alt=''
+						width={240}
+						height={240}
+					/>
+					<h1 className={styles['hero-heading']}>Raju</h1>
+				</div>
+
+				<div className={styles['hero-subheading-container']}>
+					<h2 className={styles['hero-subheading']}>Web Developer</h2>
+					<h2 className={styles['hero-subheading']}>Living in Kerala</h2>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default HeroBody
