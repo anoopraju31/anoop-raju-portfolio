a2900c4c63a9ebe0961869f7d360d6c7a773b4b0
anoopraju31 <anoopraju31@gmail.com>
2023-12-03 13:35:02 +0530

    Hero Mask Component - styled

--- src/app/(home)/sections/hero/Hero.tsx
+++ src/app/(home)/sections/hero/Hero.tsx
@@ -1,10 +1,12 @@
 import HeroBody from './HeroBody'
+import HeroMask from './HeroMask'
 import styles from './hero.module.css'
 
 const Hero = () => {
 	return (
 		<section className={styles.main}>
 			<HeroBody />
+			<HeroMask />
 		</section>
 	)
 }
--- src/app/(home)/sections/hero/HeroMask.tsx
+++ src/app/(home)/sections/hero/HeroMask.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import styles from './hero.module.css'
 
 const HeroMask = () => {
-	return <div>HeroMask</div>
+	return (
+		<div className={styles.mask}>
+			<div className={styles['hero-container']}>
+				<div className={styles['hero-heading-container']}>
+					<h1 className={styles['hero-heading']}>Anoop</h1>
+
+					<div className={styles['hero-image']} />
+					<h1 className={styles['hero-heading']}>Raju</h1>
+				</div>
+
+				<div className={styles['hero-subheading-container']}>
+					<h2 className={styles['hero-subheading']}>Web Developer</h2>
+					<h2 className={styles['hero-subheading']}>Living in Kerala</h2>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default HeroMask
