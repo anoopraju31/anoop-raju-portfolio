557bb73907e0d6f442ca201641c26cda6b7ee5ac
anoopraju31 <anoopraju31@gmail.com>
2023-12-03 22:43:28 +0530

    home Page - Project section sticky project added

--- src/app/(home)/sections/Projects.tsx
+++ src/app/(home)/sections/Projects.tsx
@@ -4,20 +4,20 @@ import ProjectCard from '@/components/ProjectCard'
 const Projects = () => {
 	return (
 		<section className='pb-10 z-50'>
-			<div className='py-5 px-[10px] flex flex-col lg:flex-row gap-4'>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+			<div className='relative py-5 px-[10px] flex flex-col lg:flex-row gap-4'>
+				<div className='sticky top-32 w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
 					<ProjectCard img='/dropbox.png' />
 				</div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+				<div className='sticky top-32 w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
 					<ProjectCard img='/eat-curious.png' />
 				</div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+				<div className='sticky top-32 w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
 					<ProjectCard img='/netflix-gpt.png' />
 				</div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+				<div className='sticky top-32 w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
 					<ProjectCard img='/nike.png' />
 				</div>
-				<div className='w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
+				<div className='sticky top-32 w-full lg:w-[20vw] lg:hover:w-[50vw] lg:hover:flex-shrink-0 h-[50vh] lg:h-[90vh] transition-width ease-in-out duration-300'>
 					<ProjectCard img='/summerize.png' />
 				</div>
 			</div>
