3230a5ccd72474181f1be934ad84206059f8a62a
anoopraju31 <anoopraju31@gmail.com>
2023-12-04 08:40:40 +0530

    Header Component - Nav Menu Close on route change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,23 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Gantari } from 'next/font/google'
 import { CgClose, CgMenu } from 'react-icons/cg'
 import NavMenu from './NavMenu'
 import { AnimatePresence } from 'framer-motion'
+import { usePathname } from 'next/navigation'
 
 const gantari = Gantari({ weight: '400', subsets: ['latin'] })
 
 const Header = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+	const pathname = usePathname()
 	const toggleMenu = () => setIsMenuOpen((prev) => !prev)
+
+	useEffect(() => {
+		setIsMenuOpen(false)
+	}, [pathname])
 
 	return (
 		<>
