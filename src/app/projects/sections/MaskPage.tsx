46217eb2efa323f4f3835a60af8613cf5cadfad2
anoopraju31 <anoopraju31@gmail.com>
2023-12-06 16:28:25 +0530

    Projects masks page - mask hover resize added

--- src/app/projects/sections/MaskPage.tsx
+++ src/app/projects/sections/MaskPage.tsx
@@ -1,26 +1,40 @@
 'use client'
 
 import { useMouse } from '@uidotdev/usehooks'
-import { Ref } from 'react'
+import { Ref, useState } from 'react'
 import { motion } from 'framer-motion'
 import styles from './projects.module.css'
 
 const MaskPage = () => {
+	const [isHover, setIshover] = useState<boolean>(false)
 	const [mouse, ref] = useMouse()
 	const { x, y } = mouse
+	const size = isHover ? 120 : 60
+
+	const handleMouseEnter = () => setIshover(true)
+	const handleMouseLeave = () => setIshover(false)
 
 	return (
 		<motion.div
 			ref={ref as Ref<HTMLDivElement> | undefined}
+			initial={{
+				WebkitMaskPosition: '-100px -100px',
+				WebkitMaskSize: '0px',
+			}}
 			animate={{
-				WebkitMaskPosition: `${x - 30}px ${y - 30}px`,
-				WebkitMaskSize: `60px`,
+				WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+				WebkitMaskSize: `${size}px`,
 			}}
 			transition={{ type: 'tween', ease: 'backOut', duration: 0.1 }}
 			className={styles.mask}>
 			<section className={styles.section}>
 				<div className={styles['heading-container']}>
-					<h1 className={styles.heading}>All Portfolio</h1>
+					<h1
+						onMouseEnter={handleMouseEnter}
+						onMouseLeave={handleMouseLeave}
+						className={styles.heading}>
+						All Portfolio
+					</h1>
 				</div>
 
 				<div className={styles['projects-container']}>
