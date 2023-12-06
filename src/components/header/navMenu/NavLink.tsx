65da26881c8988b28fe37dad60fe61f0a3410bd2
anoopraju31 <anoopraju31@gmail.com>
2023-12-06 18:28:50 +0530

    Nav Link Component - created

---
+++ src/components/header/navMenu/NavLink.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import { showAnimation, showTransition } from '@/utills/animations'
+
+type NavLinkProps = {
+	title: string
+	link: string
+}
+
+const NavLink = (props: NavLinkProps) => {
+	const { title, link } = props
+	return (
+		<Link
+			href={link}
+			className='text-[8vh] lg:text-[15vh] font-bold leading-none uppercase hover:scale-75 hover:text-black transition-all duration-300'>
+			<motion.span
+				variants={showAnimation}
+				initial='initial'
+				whileInView='animate'
+				exit='exit'
+				viewport={{ once: true }}
+				transition={showTransition}>
+				{title}
+			</motion.span>
+		</Link>
+	)
+}
+
+export default NavLink
--- src/components/header/navMenu/NavMenu.tsx
+++ src/components/header/navMenu/NavMenu.tsx
@@ -1,31 +1,7 @@
-import Link from 'next/link'
 import { motion } from 'framer-motion'
-import { menuSlide, showAnimation, showTransition } from '@/utills/animations'
+import { menuSlide } from '@/utills/animations'
 import Curve from '../Curve'
-
-type NavLinkProps = {
-	title: string
-	link: string
-}
-
-const NavLinks = (props: NavLinkProps) => {
-	const { title, link } = props
-	return (
-		<Link
-			href={link}
-			className='text-[8vh] lg:text-[15vh] font-bold leading-none uppercase hover:scale-75 hover:text-black transition-all duration-300'>
-			<motion.span
-				variants={showAnimation}
-				initial='initial'
-				whileInView='animate'
-				exit='exit'
-				viewport={{ once: true }}
-				transition={showTransition}>
-				{title}
-			</motion.span>
-		</Link>
-	)
-}
+import NavLink from './NavLink'
 
 const NavMenu = () => {
 	return (
@@ -36,9 +12,9 @@ const NavMenu = () => {
 			exit='exit'
 			className='fixed top-0 left-0 right-0 z-40 h-screen bg-light-green flex justify-center items-center'>
 			<nav className='group flex flex-col justify-center items-center text-black'>
-				<NavLinks title='Home' link='/' />
-				<NavLinks title='Projects' link='/projects' />
-				<NavLinks title='Contact' link='/contact' />
+				<NavLink title='Home' link='/' />
+				<NavLink title='Projects' link='/projects' />
+				<NavLink title='Contact' link='/contact' />
 			</nav>
 			<Curve />
 		</motion.div>
