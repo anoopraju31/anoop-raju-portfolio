ec9867f535decdf8fd88c2808b6922095becc002
anoopraju31 <anoopraju31@gmail.com>
2023-12-06 16:18:08 +0530

    Projects masks page - styled

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,11 @@
 import ProjectCard from '@/components/projectCard/ProjectCard'
 import RegularPage from './sections/RegularPage'
+import MaskPage from './sections/MaskPage'
 
 const ProjectsPage = () => {
 	return (
 		<main className='relative bg-black text-white'>
+			<MaskPage />
 			<RegularPage />
 		</main>
 	)
--- src/app/projects/sections/MaskPage.tsx
+++ src/app/projects/sections/MaskPage.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import styles from './projects.module.css'
 
 const MaskPage = () => {
-	return <div>MaskPage</div>
+	return (
+		<div className={styles.mask}>
+			<section className={styles.section}>
+				<div className={styles['heading-container']}>
+					<h1 className={styles.heading}>All Portfolio</h1>
+				</div>
+
+				<div className={styles['projects-container']}>
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+					<div className={styles['mask-project-card']} />
+				</div>
+			</section>
+		</div>
+	)
 }
 
 export default MaskPage
