855ee6d77292c9c51401610f55af6475e8a03ea8
anoopraju31 <anoopraju31@gmail.com>
2023-12-06 18:30:53 +0530

    Nav Menu Regular - styled

--- src/components/header/navMenu/NavMenu.tsx
+++ src/components/header/navMenu/NavMenu.tsx
@@ -1,23 +1,12 @@
-import { motion } from 'framer-motion'
-import { menuSlide } from '@/utills/animations'
-import Curve from '../Curve'
-import NavLink from './NavLink'
+import NavMenuMask from './NavMenuMask'
+import NavMenuRegular from './NavMenuRegular'
 
 const NavMenu = () => {
 	return (
-		<motion.div
-			variants={menuSlide}
-			initial='initial'
-			animate='enter'
-			exit='exit'
-			className='fixed top-0 left-0 right-0 z-40 h-screen bg-light-green flex justify-center items-center'>
-			<nav className='group flex flex-col justify-center items-center text-black'>
-				<NavLink title='Home' link='/' />
-				<NavLink title='Projects' link='/projects' />
-				<NavLink title='Contact' link='/contact' />
-			</nav>
-			<Curve />
-		</motion.div>
+		<div className='relative'>
+			<NavMenuMask />
+			<NavMenuRegular />
+		</div>
 	)
 }
 
--- src/components/header/navMenu/NavMenuRegular.tsx
+++ src/components/header/navMenu/NavMenuRegular.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { menuSlide } from '@/utills/animations'
+import Curve from '../Curve'
+import NavLink from './NavLink'
 
 const NavMenuRegular = () => {
-	return <div>NavMenuRegular</div>
+	return (
+		<motion.div
+			variants={menuSlide}
+			initial='initial'
+			animate='enter'
+			exit='exit'
+			className='fixed top-0 left-0 right-0 z-40 h-screen bg-light-green flex justify-center items-center'>
+			<nav className='group flex flex-col justify-center items-center text-black'>
+				<NavLink title='Home' link='/' />
+				<NavLink title='Projects' link='/projects' />
+				<NavLink title='Contact' link='/contact' />
+			</nav>
+			<Curve />
+		</motion.div>
+	)
 }
 
 export default NavMenuRegular
