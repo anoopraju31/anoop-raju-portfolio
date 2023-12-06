de2bddb89f59d7ac517a56f3ca44665fda708143
anoopraju31 <anoopraju31@gmail.com>
2023-12-06 16:20:51 +0530

    Projects masks page - mask added

--- src/app/projects/sections/MaskPage.tsx
+++ src/app/projects/sections/MaskPage.tsx
@@ -1,8 +1,23 @@
+'use client'
+
+import { useMouse } from '@uidotdev/usehooks'
+import { Ref } from 'react'
+import { motion } from 'framer-motion'
 import styles from './projects.module.css'
 
 const MaskPage = () => {
+	const [mouse, ref] = useMouse()
+	const { x, y } = mouse
+
 	return (
-		<div className={styles.mask}>
+		<motion.div
+			ref={ref as Ref<HTMLDivElement> | undefined}
+			animate={{
+				WebkitMaskPosition: `${x - 30}px ${y - 30}px`,
+				WebkitMaskSize: `60px`,
+			}}
+			transition={{ type: 'tween', ease: 'backOut', duration: 0.1 }}
+			className={styles.mask}>
 			<section className={styles.section}>
 				<div className={styles['heading-container']}>
 					<h1 className={styles.heading}>All Portfolio</h1>
@@ -22,7 +37,7 @@ const MaskPage = () => {
 					<div className={styles['mask-project-card']} />
 				</div>
 			</section>
-		</div>
+		</motion.div>
 	)
 }
 
