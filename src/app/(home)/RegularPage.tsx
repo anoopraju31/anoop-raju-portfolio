b8fe6a4d7580a1c908b2bd5eb794c450c451cc6d
anoopraju31 <anoopraju31@gmail.com>
2023-12-21 19:06:53 +0530

    Home Page - regular view created

---
+++ src/app/(home)/RegularPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './sections/hero/Hero'
+import About from './sections/about/About'
+
+const RegularPage = () => {
+	return (
+		<div>
+			<Hero />
+			<About />
+		</div>
+	)
+}
+
+export default RegularPage
