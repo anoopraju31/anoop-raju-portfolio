2fedd76c2a08ff869075b562d9ed73d2d2a7707f
anoopraju31 <anoopraju31@gmail.com>
2023-12-24 22:27:33 +0530

    Constants - projects added

---
+++ src/utills/constants.ts
@@ -0,0 +1,12 @@
+type Project = {
+	id: number
+	img: string
+}
+
+export const projects: Project[] = [
+	{ id: 0, img: '/dropbox.png' },
+	{ id: 1, img: '/eat-curious.png' },
+	{ id: 2, img: '/summerize.png' },
+	{ id: 3, img: '/netflix-gpt.png' },
+	{ id: 4, img: '/nike.png' },
+]
