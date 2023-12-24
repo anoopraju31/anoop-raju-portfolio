297571891b50984693f3c5530aff3f5608df3653
anoopraju31 <anoopraju31@gmail.com>
2023-12-24 22:25:52 +0530

    Redux Store and provider added

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import textHoverReducer from './features/textHoverSlice'
+import projectCardhoverReducer from './features/projectCardSlice'
+import navbarReducer from './features/navbarSlice'
+
+export const store = configureStore({
+	reducer: {
+		textHover: textHoverReducer,
+		projectCardHover: projectCardhoverReducer,
+		navbar: navbarReducer,
+	},
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
