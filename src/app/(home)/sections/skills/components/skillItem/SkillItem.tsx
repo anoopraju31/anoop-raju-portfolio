12c394716f1c8cd887f1d295541211b4915bbf26
anoopraju31 <anoopraju31@gmail.com>
2023-12-25 17:38:28 +0530

    Home Page- Skill section: Skill item component added

---
+++ src/app/(home)/sections/skills/components/skillItem/SkillItem.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useRef } from 'react'
+import { useTransform, motion, useScroll, cubicBezier } from 'framer-motion'
+import styles from './skillItem.module.css'
+
+type Props = {
+	skill: string
+}
+
+const SkillItem = (props: Props) => {
+	const { skill } = props
+
+	const container = useRef<HTMLDivElement | null>(null)
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ['start 0.9', 'start 0.2'],
+	})
+	const width = useTransform(scrollYProgress, [0, 1], ['0%', '100%'], {
+		ease: cubicBezier(0.17, 0.67, 0.83, 0.67),
+	})
+
+	return (
+		<div ref={container} className={styles.container}>
+			<motion.p style={{ width }} className={styles['mask-text']}>
+				{skill}
+			</motion.p>
+			<p className={styles['main-text']}>{skill}</p>
+		</div>
+	)
+}
+
+export default SkillItem
