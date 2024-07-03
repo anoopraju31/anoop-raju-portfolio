c9c030d2a0ad8974231ea2ab629995a6a0c09acc
anoopraju31 <anoopraju31@gmail.com>
2024-07-03 15:52:23 +0530

    Blogs page - initia

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+const BlogsPage: FC = () => {
+	return (
+		<main className='relative min-h-screen bg-black text-white'>BlogsPage</main>
+	)
+}
+
+export default BlogsPage
