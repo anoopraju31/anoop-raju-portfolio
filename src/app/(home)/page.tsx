66dd17fd91e5600974bb0ddf221492d4d6c21b7c
anoopraju31 <anoopraju31@gmail.com>
2024-07-03 16:27:15 +0530

    Particles added to Home Page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,20 @@
+'use client'
+import { useEffect } from 'react'
 import MaskPage from './MaskPage'
 import RegularPage from './RegularPage'
+import { Particles } from '@/components/Particles'
 
 export default function Home() {
+	useEffect(() => {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+		})
+	}, [])
+
 	return (
-		<main className='bg-dark-blue text-white relative '>
+		<main className='bg-dark-blue text-white relative z-10'>
+			<Particles className='fixed inset-0 -z-10 h-screen' />
 			<MaskPage />
 			<RegularPage />
 		</main>
