8c54e46ab6b7ca43ad20a4c91fc1232615541957
anoopraju31 <anoopraju31@gmail.com>
2024-07-04 11:43:43 +0530

    Home Page - Blogs Section: Body layout added

--- src/app/(home)/sections/blogs/Blog.tsx
+++ src/app/(home)/sections/blogs/Blog.tsx
@@ -1,7 +1,27 @@
-import { type FC } from 'react'
+import React, { FC } from 'react'
+import styles from './blog.module.css'
+import Link from 'next/link'
 
 const Blog: FC = () => {
-	return <div>Blog</div>
+	return (
+		<section className={styles.section} aria-label='skills'>
+			<div className={styles.container}>
+				<div className={styles['heading-container']}>
+					<h2 className={styles.heading}> Blogs </h2>
+				</div>
+
+				<div className={styles['outter-container']}>
+					<div className={styles['inner-container']}></div>
+
+					<div className={styles['link-container']}>
+						<Link href='/blogs' className={styles['link']}>
+							View More Blogs
+						</Link>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Blog
