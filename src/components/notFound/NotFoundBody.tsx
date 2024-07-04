4c8cb826748d6f0eac4b033dca9a5d36f1bb4260
anoopraju31 <anoopraju31@gmail.com>
2024-07-04 21:50:02 +0530

    NotFound Page added

---
+++ src/components/notFound/NotFoundBody.tsx
@@ -0,0 +1,21 @@
+import { type FC } from 'react'
+import Link from 'next/link'
+import styles from './styles.module.css'
+
+const NotFoundBody: FC = () => {
+	return (
+		<section className={styles.body}>
+			<div className={styles.container}>
+				<h1 className={styles.heading}>
+					<span className='sr-only'>Error</span> 404
+				</h1>
+				<p className={styles.desc}>Sorry, we couldn&apos;t find this page.</p>
+				<Link href='/' className={styles.link}>
+					Back to homepage
+				</Link>
+			</div>
+		</section>
+	)
+}
+
+export default NotFoundBody
