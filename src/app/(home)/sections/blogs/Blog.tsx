b7b807645ad5a21fab9bfc6e1adc85fa6803a001
anoopraju31 <anoopraju31@gmail.com>
2024-07-04 22:19:10 +0530

    Home Page - Blog Section: refactor

--- src/app/(home)/sections/blogs/Blog.tsx
+++ src/app/(home)/sections/blogs/Blog.tsx
@@ -1,17 +1,22 @@
-import React, { FC } from 'react'
-import styles from './blog.module.css'
+import { type FC } from 'react'
 import Link from 'next/link'
+import BlogCard from '@/components/blogCard/BlogCard'
+import styles from './blog.module.css'
 
 const Blog: FC = () => {
 	return (
 		<section className={styles.section} aria-label='skills'>
 			<div className={styles.container}>
 				<div className={styles['heading-container']}>
-					<h2 className={styles.heading}> Blogs </h2>
+					<h2 className={styles.heading}> Latest from Blogs </h2>
 				</div>
 
 				<div className={styles['outter-container']}>
-					<div className={styles['inner-container']}></div>
+					<div className={styles['inner-container']}>
+						<BlogCard />
+						<BlogCard />
+						<BlogCard />
+					</div>
 
 					<div className={styles['link-container']}>
 						<Link href='/blogs' className={styles['link']}>
--- src/app/(home)/sections/blogs/BlogMask.tsx
+++ src/app/(home)/sections/blogs/BlogMask.tsx
@@ -1,7 +1,54 @@
-import React, { FC } from 'react'
+'use client'
+
+import { type FC } from 'react'
+import useAppDispatch from '@/app/hooks/useAddDispatch'
+import Link from 'next/link'
+import BlogCardMask from '@/components/blogCard/BlogCardMask'
+import { mouseEnter, mouseLeave } from '@/app/features/textHoverSlice'
+import styles from './blog.module.css'
 
 const BlogMask: FC = () => {
-	return <div>BlogMask</div>
+	const dispatch = useAppDispatch()
+
+	const handleMouseEnter = () => dispatch(mouseEnter())
+	const handleMouseLeave = () => dispatch(mouseLeave())
+
+	return (
+		<div className={styles.section} aria-label='skills'>
+			<div className={styles.container}>
+				<div className={styles['heading-container']}>
+					<h2
+						onMouseEnter={handleMouseEnter}
+						onMouseLeave={handleMouseLeave}
+						className={styles.heading}>
+						{' '}
+						Latest from Blogs{' '}
+					</h2>
+				</div>
+
+				<div className={styles['outter-container']}>
+					<div
+						onMouseEnter={handleMouseEnter}
+						onMouseLeave={handleMouseLeave}
+						className={styles['inner-container']}>
+						<BlogCardMask />
+						<BlogCardMask />
+						<BlogCardMask />
+					</div>
+
+					<div className={styles['link-container']}>
+						<Link
+							onMouseEnter={handleMouseEnter}
+							onMouseLeave={handleMouseLeave}
+							href='/blogs'
+							className={styles['link']}>
+							View More Blogs
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default BlogMask
