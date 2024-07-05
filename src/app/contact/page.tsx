51db14501f8370cebf5071701921cc579f079d71
anoopraju31 <anoopraju31@gmail.com>
2024-07-05 12:10:51 +0530

    Contact Page - metadata added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,8 +2,13 @@ import { type FC } from 'react'
 import { PT_Sans } from 'next/font/google'
 import { Particles } from '@/components/Particles'
 import RegularPage from './RegularPage'
+import { Metadata } from 'next'
 
 const ptSans = PT_Sans({ weight: '400', subsets: ['latin'] })
+
+export const metadata: Metadata = {
+	title: 'Contact Me',
+}
 
 const ContactPage: FC = () => {
 	return (
