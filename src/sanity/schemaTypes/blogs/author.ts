2d83c887cf56058cb5b43de0c476ea988ecd9df6
anoop raju <anoopraju31@gmail.com>
2024-12-02 00:03:11 +0530

    Author type added

---
+++ src/sanity/schemaTypes/blogs/author.ts
@@ -0,0 +1,40 @@
+import { defineField, defineType } from 'sanity'
+import { UserIcon } from '@sanity/icons'
+
+export const authorType = defineType({
+	name: 'author',
+	title: 'Author',
+	icon: UserIcon,
+	type: 'document',
+	fields: [
+		defineField({
+			name: 'name',
+			title: 'Name',
+			type: 'string',
+			validation: (Rule) => Rule.required()
+		}),
+		defineField({
+			name: 'picture',
+			title: 'Picture',
+			type: 'image',
+			fields: [
+				{
+					name: 'alt',
+					title: 'Alternative text',
+					type: 'string',
+					description: 'Important for SEO and accessibility.',
+					validation: (rule) =>
+						rule.custom((alt, context) => {
+							const document = context.document as { picture?: { asset?: { _ref?: string } } }
+
+							return document.picture?.asset?._ref && !alt ? 'Required' : true
+						})
+				}
+			],
+			options: {
+				hotspot: true
+			},
+			validation: (rule) => rule.required()
+		})
+	]
+})
