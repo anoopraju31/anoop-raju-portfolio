263ca2064785e5d82f78013ae94deb7a2efa333e
anoop raju <anoopraju31@gmail.com>
2024-12-04 11:02:11 +0530

    Sanity Schema for project added

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 import authorType from '@/sanity/schemaTypes/blogs/author'
 import tags from '@/sanity/schemaTypes/blogs/tags'
 import contactMe from './contactMe'
+import projects from './projects'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [authorType, tags, contactMe]
+	types: [authorType, tags, contactMe, projects]
 }
