951d79a6c5661426033c0d2ca775ec7937998437
anoop raju <anoopraju31@gmail.com>
2025-02-11 15:14:09 +0530

    Chore: Blog card link added

--- src/components/blogCard/index.tsx
+++ src/components/blogCard/index.tsx
@@ -3,6 +3,7 @@ import { Blogs } from '../../../types'
 import { format } from 'date-fns'
 import { PT_Sans } from 'next/font/google'
 import styles from './styles.module.css'
+import Link from 'next/link'
 
 type Props = {
 	isMask?: boolean
@@ -13,15 +14,17 @@ const ptSans = PT_Sans({ weight: '400', subsets: ['latin'] })
 
 const BlogCard: FC<Props> = ({ isMask, blog }) => {
 	return (
-		<div className={` ${isMask ? styles.card__mask : styles.card} ${ptSans.className}`}>
-			<div className={styles.badge}>
-				<span>{blog.tags?.[0] ?? 'Design'}</span>
+		<Link href={`/blogs/${blog.slug}`}>
+			<div className={` ${isMask ? styles.card__mask : styles.card} ${ptSans.className}`}>
+				<div className={styles.badge}>
+					<span>{blog.tags?.[0] ?? 'Design'}</span>
+				</div>
+
+				<h3 className={styles.card__title}> {blog.title} </h3>
+
+				<div className={styles.card__date}>{format(new Date(blog.date), 'LLLL	d, yyyy')} </div>
 			</div>
-
-			<h3 className={styles.card__title}> {blog.title} </h3>
-
-			<div className={styles.card__date}>{format(new Date(blog.date), 'LLLL	d, yyyy')} </div>
-		</div>
+		</Link>
 	)
 }
 
