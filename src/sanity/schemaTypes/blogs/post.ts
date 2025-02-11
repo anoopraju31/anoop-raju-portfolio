4d31fe1ca260c46a46aa27d7c3572e42eaff5208
anoop raju <anoopraju31@gmail.com>
2025-02-11 13:16:22 +0530

    Chore: Sanity schema updated

--- src/sanity/schemaTypes/blogs/post.ts
+++ src/sanity/schemaTypes/blogs/post.ts
@@ -1,102 +1,161 @@
-import { formatDate } from '@/utills'
+import { DocumentTextIcon } from '@sanity/icons'
+import { format, parseISO } from 'date-fns'
 import { defineField, defineType } from 'sanity'
+import { authorType } from './author'
+import { tagType } from './tags'
 
-export default defineType({
+export const postType = defineType({
 	name: 'post',
 	title: 'Post',
+	icon: DocumentTextIcon,
 	type: 'document',
 	fields: [
 		defineField({
 			name: 'title',
 			title: 'Title',
 			type: 'string',
-			validation: (Rule) => Rule.required().error('Title is required')
+			validation: (rule) => rule.required()
 		}),
 		defineField({
 			name: 'slug',
 			title: 'Slug',
 			type: 'slug',
+			description: 'It for Url Navigation and SEO  ex. blog-1 than url is blogs.infigon.in/blog-1',
 			options: {
 				source: 'title',
 				maxLength: 96,
 				isUnique: (value, context) => context.defaultIsUnique(value, context)
 			},
-			validation: (Rule) => Rule.required().error('Slug is required')
+			validation: (rule) => rule.required()
 		}),
 		defineField({
-			name: 'publishedAt',
-			title: 'Published At',
-			type: 'datetime',
-			initialValue: () => new Date().toISOString(),
+			name: 'content',
+			title: 'Content',
+			type: 'array',
+			description: 'This is the main Content of Blog (Formatting is preserved)',
+			of: [
+				{
+					type: 'block'
+				},
+				{
+					type: 'image',
+					options: { hotspot: true },
+					fields: [
+						{
+							name: 'alt',
+							title: 'Alternative Text',
+							type: 'string',
+							options: { isHighlighted: true },
+							validation: (Rule) =>
+								Rule.required().error('Alternative text is required for accessibility.')
+						},
+						{
+							name: 'caption',
+							title: 'Caption',
+							type: 'string',
+							options: { isHighlighted: true }
+						}
+					]
+				},
+				{
+					name: 'table',
+					type: 'table'
+				}
+			],
 			validation: (Rule) => Rule.required()
 		}),
 		defineField({
-			name: 'updatedAt',
-			title: 'Updated At',
-			type: 'datetime'
+			name: 'conclusion',
+			title: 'Conclusion',
+			type: 'array',
+			description:
+				'(Optional) This is the conclusion of Blog (Formatting is preserved & do not add heading - Conclusion) ',
+			of: [{ type: 'block' }]
 		}),
 		defineField({
-			name: 'author',
-			title: 'Author',
-			type: 'reference',
-			to: [{ type: 'author' }],
-			validation: (Rule) => Rule.required().error('Author is required')
+			name: 'readTime',
+			type: 'number',
+			title: 'Read Time (in minutes)',
+			description: 'Estimated time to read the blog post in minutes',
+			validation: (Rule) => Rule.positive().integer().min(1).max(1440) // Example limits: 1 minute to 24 hours
 		}),
-		defineField({
-			name: 'tags',
-			title: 'Tags',
-			type: 'reference',
-			to: [{ type: 'tags' }],
-			validation: (Rule) => Rule.required().error('Tags are required')
-		}),
+
 		defineField({
 			name: 'coverImage',
 			title: 'Cover Image',
 			type: 'image',
-			fields: [
-				{
-					name: 'alt',
-					title: 'Alternative text',
-					type: 'string',
-					description: 'Important for SEO and accessibility.',
-					validation: (rule) =>
-						rule.custom((alt, context) => {
-							const document = context.document as { coverImage?: { asset?: { _ref?: string } } }
-
-							return document.coverImage?.asset?._ref && !alt
-								? 'Alternative text is required when an image is added.'
-								: true
-						})
-				}
-			],
 			options: {
 				hotspot: true
 			},
-			validation: (rule) => rule.required().error('Cover image is required')
+			fields: [
+				{
+					name: 'alt',
+					type: 'string',
+					title: 'Alternative text',
+					description: 'Important for SEO and accessiblity.',
+					validation: (rule) => {
+						return rule.custom((alt, context) => {
+							const document = context.document as { picture?: { asset?: { _ref?: string } } }
+							if (document.picture?.asset?._ref && !alt) {
+								return 'Required'
+							}
+							return true
+						})
+					}
+				}
+			],
+			validation: (rule) => rule.required()
 		}),
 		defineField({
-			name: 'excerpt',
-			title: 'Excerpt',
+			name: 'date',
+			title: 'Date',
+			type: 'datetime',
+			initialValue: () => new Date().toISOString()
+		}),
+		defineField({
+			name: 'author',
+			title: 'Author',
+			description: 'Want to add other author : Go to Author Section and add new author',
+			type: 'reference',
+			to: [{ type: authorType.name }]
+		}),
+
+		defineField({
+			name: 'keywords',
+			title: 'Keywords',
+			type: 'array',
+			of: [{ type: 'string' }]
+		}),
+		defineField({
+			name: 'seoTitle',
+			title: 'SEO Title',
+			type: 'string',
+			validation: (Rule) => Rule.max(60).warning('Keep the SEO title under 60 characters.')
+		}),
+		defineField({
+			name: 'seoDescription',
+			title: 'SEO Description',
 			type: 'text',
-			validation: (Rule) => Rule.required().error('Excerpt is required')
+			validation: (Rule) => Rule.max(160).warning('Keep the SEO description under 160 characters.')
+		}),
+		defineField({
+			name: 'tags',
+			title: 'Tags',
+			type: 'array',
+			of: [{ type: 'reference', to: { type: tagType.name } }]
 		})
-		// defineField({
-		// 	name: 'content',
-		// 	title: 'Content',
-		// 	type: 'reference',
-		// 	to: [{ type: 'blockContent' }],
-		// 	validation: (Rule) => Rule.required().error('Content is required')
-		// })
 	],
 	preview: {
 		select: {
 			title: 'title',
 			author: 'author.name',
-			date: 'publishedAt',
+			date: 'date',
 			media: 'coverImage'
 		},
-		prepare({ title, author, date, media }) {
-			const subtitles = [author && `by ${author}`, date && `on ${formatDate(date)}`].filter(Boolean)
+		prepare({ title, media, author, date }) {
+			const subtitles = [author && `by ${author}`, date && `on ${format(parseISO(date), 'LLL d, yyyy')}`].filter(
+				Boolean
+			)
 
 			return { title, media, subtitle: subtitles.join(' ') }
 		}
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,11 +1,9 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import authorType from '@/sanity/schemaTypes/blogs/author'
-import tags from '@/sanity/schemaTypes/blogs/tags'
 import contactMe from './contactMe'
 import projects from './projects'
-import blockContent from './blogs/blockContent'
-import post from './blogs/post'
+import { authorType } from './blogs/author'
+import { tagType } from './blogs/tags'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [authorType, tags, contactMe, projects]
+	types: [authorType, tagType, contactMe, projects]
 }
