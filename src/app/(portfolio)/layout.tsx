5d2f0b430a9491d05a92d17c512f9d4a8b46b7a2
anoop raju <anoopraju31@gmail.com>
2025-04-07 00:44:57 +0530

    Chore: metadata updated

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,18 +1,8 @@
-import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 import ScrollToTopOnReload from '@/components/ScrollToTopOnReload'
 import Header from '@/components/header/Header'
 import Footer from '@/components/footer'
 import { Toaster } from 'sonner'
-
-export const metadata: Metadata = {
-	title: {
-		default: 'Anoop Raju - Portfolio',
-		template: 'Anoop Raju | %s'
-	},
-	description:
-		"Discover Anoop's portfolio - a showcase of creative works and projects reflecting passion and expertise in ."
-}
 
 type Props = {
 	children: ReactNode
