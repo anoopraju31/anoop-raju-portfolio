c845d133e0068a4ea175240677da0ca53ea27481
anoop raju <anoopraju31@gmail.com>
2025-04-07 15:40:07 +0530

    Chore: Hero section text update

--- src/app/(portfolio)/(home)/sections/hero/Hero.tsx
+++ src/app/(portfolio)/(home)/sections/hero/Hero.tsx
@@ -9,22 +9,24 @@ const Hero = () => {
 	return (
 		<section className={styles.body}>
 			<div className={styles['hero-container']}>
-				<div className={styles['hero-heading-container']}>
-					<motion.h1
+				<h1 className={styles['hero-heading-container']}>
+					<motion.span
 						variants={slideToView}
 						initial='initial'
 						whileInView='animate'
 						viewport={{ once: true }}
-						className={styles['hero-heading']}>
+						className={styles['hero-heading']}
+					>
 						Anoop
-					</motion.h1>
+					</motion.span>
 
 					<motion.div
 						variants={slideToView}
 						initial='initial'
 						whileInView='animate'
 						viewport={{ once: true }}
-						className='flex-shrink-0 overflow-hidden w-[var(--hero-image-width)] xl:w-[240px] h-[var(--hero-image-width)] xl:h-[240px]'>
+						className='flex-shrink-0 overflow-hidden w-[var(--hero-image-width)] xl:w-[240px] h-[var(--hero-image-width)] xl:h-[240px]'
+					>
 						<Image
 							src='/anoop-raju.jpg'
 							className='w-full h-full rounded-full  object-cover'
@@ -34,35 +36,38 @@ const Hero = () => {
 						/>
 					</motion.div>
 
-					<motion.h1
+					<motion.span
 						variants={slideToView}
 						initial='initial'
 						whileInView='animate'
 						viewport={{ once: true }}
-						className={styles['hero-heading']}>
+						className={styles['hero-heading']}
+					>
 						Raju
-					</motion.h1>
-				</div>
+					</motion.span>
+				</h1>
 
 				<div className={styles['hero-subheading-container']}>
-					<motion.h2
+					<motion.p
 						variants={slideToView}
 						initial='initial'
 						whileInView='animate'
 						viewport={{ once: true }}
 						transition={{ delay: 0.5 }}
-						className={styles['hero-subheading']}>
+						className={styles['hero-subheading']}
+					>
 						Full Stack Developer
-					</motion.h2>
-					<motion.h2
+					</motion.p>
+					<motion.p
 						variants={slideToView}
 						initial='initial'
 						whileInView='animate'
 						viewport={{ once: true }}
 						transition={{ delay: 0.5 }}
-						className={styles['hero-subheading']}>
-						Living in Kerala
-					</motion.h2>
+						className={styles['hero-subheading']}
+					>
+						Living in Kerala, India
+					</motion.p>
 				</div>
 			</div>
 		</section>
